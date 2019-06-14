0d4db3e356101d13ff320b5d0cf432f4b4ea70c9
aspiringguru <bmatthewtaylor@gmail.com>
2019-06-14 16:54:45 +1000

    wip, before refactoring_

--- DHT22MQTTDemo/DHT22MQTTDemo.ino
+++ DHT22MQTTDemo/DHT22MQTTDemo.ino
@@ -1,3 +1,16 @@
+/*
+ * edit mosquitto.conf to allow local connections & nominate port
+ * ie: listener 1883 0.0.0.0
+ * open mosquitto, mosquitto _sub & mosquitto _pub in three different cmd shells to see interaction
+ * mosquitto -v -c mosquitto.conf
+ * ipconfig to check ip number of mostquitto server
+ * mosquitto_sub -p 1883 -d -t testTopic
+ * mosquitto_pub -p 1883 -d -t testTopic -m "Hello world!"
+ * 
+ * https://xylem.aegean.gr/~modestos/mo.blog/esp32-send-dht-to-mqtt-and-deepsleep/
+ * 
+ */
+
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <DHT.h>
@@ -19,6 +32,18 @@ const int mqttPort = 1883;
 //dont need to use username and password
 //const char* mqttUser = "yourMQTTuser";
 //const char* mqttPassword = "yourMQTTpassword";
+
+#define temperature_topic "topic/temp1"       //Topic temperature
+#define humidity_topic "topic/humid1"         //Topic humidity
+#define debug_topic "debug"                   //Topic for debugging
+ 
+/* likely adjust these during dev/testing */
+/* definitions for deepsleep */
+#define uS_TO_S_FACTOR 1000000        /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP 900              /* Time ESP32 will go to sleep for 15 minutes (in seconds) */
+#define TIME_TO_SLEEP_ERROR 3600       /* Time to sleep in case of error (1 hour) */
+bool debug = true;             //Display log message if True
+
  
 WiFiClient espClient;
 PubSubClient client(espClient);
