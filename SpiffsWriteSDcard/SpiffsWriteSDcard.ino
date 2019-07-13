ad3bfd7817114656365425d06a9181ae8a5d1418
aspiringguru <bmatthewtaylor@gmail.com>
2019-07-13 11:04:14 +1000

    SpiffsWriteSDcard, connects to wifi, retrieves time from ntp server, formats time, writes to file, goes to sleep, repeat.

--- SpiffsWriteSDcard/SpiffsWriteSDcard.ino
+++ SpiffsWriteSDcard/SpiffsWriteSDcard.ino
@@ -10,6 +10,20 @@
 #include "config.h"
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
+uint64_t uS_TO_S_FACTOR = 1000000;
+uint64_t time2sleep = 5;//change to longer intervals later, ie 600 seconds
+
+// Save reading number on RTC memory. this is stored during deep sleep
+//https://docs.espressif.com/projects/esp-idf/en/latest/api-guides/deep-sleep-stub.html
+RTC_DATA_ATTR int readingID = 0;
+
+String dataMessage;
+
+// Variables to save date and time
+String formattedDate;
+String dayStamp;
+String timeStamp;
+
 
 // Define CS pin for the SD card module
 #define SD_CS 5
@@ -26,6 +40,17 @@ NTPClient timeClient(ntpUDP);
 
 void setup(){
   Serial.begin(115200);
+  Serial.print("ssid:");
+  Serial.println(ssid);
+  Serial.print("password:");
+  Serial.println(password);
+  Serial.print("uS_TO_S_FACTOR:");
+  //Serial.println(uS_TO_S_FACTOR);
+  print_uint64_t(uS_TO_S_FACTOR);
+  Serial.print("time2sleep:");
+  //Serial.println(time2sleep);
+  print_uint64_t(time2sleep);
+  
   WiFi.begin(ssid, password);
 
   while ( WiFi.status() != WL_CONNECTED ) {
@@ -64,20 +89,35 @@ void setup(){
   if(!file) {
     Serial.println("File doens't exist");
     Serial.println("Creating file...");
-    writeFile(SD, "/data.txt", "Reading ID, Date, Hour, Temperature \r\n");
+    writeFile(SD, "/data.txt", "Reading ID, Date, Hour \r\n");
   }
   else {
     Serial.println("File already exists");  
   }
   file.close();
 
+  Serial.print("go to sleep for ");
+  //Serial.print(time2sleep);
+  print_uint64_t(time2sleep);
+  Serial.println(" seconds");
+  // Enable Timer wake_up
+  esp_sleep_enable_timer_wakeup(uS_TO_S_FACTOR * time2sleep);
+
+  //do whatever readings and writing to file here before sleeping
+  getTimeStamp();
+  logSDCard();
+
+  // Increment readingID on every new reading
+  readingID++;
+  // Start deep sleep
+  Serial.println("DONE! Going to sleep now.");
+  esp_deep_sleep_start(); 
+
 }
 
 void loop() {
-  timeClient.update();
-
-  Serial.println(timeClient.getFormattedTime());
-  Serial.println("blah");
+  //due to deep sleep in setup() this will never run
+  Serial.println("loop() started");
   delay(1000);
 }
 
@@ -97,4 +137,71 @@ void writeFile(fs::FS &fs, const char * path, const char * message) {
     Serial.println("Write failed");
   }
   file.close();
+
+}
+
+
+// Append data to the SD card (DON'T MODIFY THIS FUNCTION)
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+// Write the sensor readings on the SD card
+void logSDCard() {
+  dataMessage = String(readingID) + "," + String(dayStamp) + "," + String(timeStamp) + "," + "\r\n";
+  Serial.print("Saving data to file : ");
+  Serial.println(dataMessage);
+  appendFile(SD, "/data.txt", dataMessage.c_str());
+}
+
+
+
+
+// Function to get date and time from NTPClient
+void getTimeStamp() {
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  // The formattedDate comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+  formattedDate = timeClient.getFormattedDate();
+  Serial.println(formattedDate);
+
+  // Extract date
+  int splitT = formattedDate.indexOf("T");
+  dayStamp = formattedDate.substring(0, splitT);
+  Serial.println(dayStamp);
+  // Extract time
+  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+  Serial.println(timeStamp);
+}
+
+
+void print_uint64_t(uint64_t num) {
+
+  char rev[128]; 
+  char *p = rev+1;
+
+  while (num > 0) {
+    *p++ = '0' + ( num % 10);
+    num/= 10;
+  }
+  p--;
+  /*Print the number which is now in reverse*/
+  while (p > rev) {
+    Serial.print(*p--);
+  }
 }
